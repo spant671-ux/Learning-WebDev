0b68824d3dcdd1b7ee71141ef609580582499142
Santosh <spant671@gmail.com>
2026-07-14 20:39:01 +0530

    added variables

--- Learning Javascript/Variables and Datatypes/index.js
+++ Learning Javascript/Variables and Datatypes/index.js
@@ -1,8 +1,8 @@
-var x =60;
-console.log(x);
+// var x =60;
+// console.log(x);
 
-var x = "hello";
-console.log();
+// var x = "hello";
+// console.log(x);
 
 // function solve(){
 //     var age = 25;
@@ -10,3 +10,17 @@ console.log();
 // }
 // solve();
 // console.log(age)
+
+// {
+// let a = 10;
+// console.log(a);
+// }
+// console.log(a)
+
+// let a = 10;
+// let a = 20; 
+
+const a = 28;
+console.log(a);
+a=20;
+console.log(a);
