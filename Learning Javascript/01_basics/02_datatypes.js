f8deef39c1ce3c7d0fca5ff8aa75b2ed3b6acc36
Santosh <spant671@gmail.com>
2026-07-16 00:50:42 +0530

    finished datatypes

--- Learning Javascript/01_basics/02_datatypes.js
+++ Learning Javascript/01_basics/02_datatypes.js
@@ -7,4 +7,23 @@ console.log(3
     3) //code readability should be high
 console.log("hello")
 
+let name= "Kendrick"
+let age= 29
+let isLoggedIn= false
+
+// number => 2 to the power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof age) //number
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
 
