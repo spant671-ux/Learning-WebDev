18644b80e6c3a4e04ef6f6fe97d3611d1e65fce1
Santosh <spant671@gmail.com>
2026-07-16 00:38:47 +0530

    started datatypes

---
+++ Learning Javascript/01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treats all JS code as newer version
+
+// alert("hello") We are using Node.js, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+console.log("hello")
+
+
