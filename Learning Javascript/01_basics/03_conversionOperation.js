d84ba988c3e302f8f15c3ced92d9c0e1708cb200
Santosh <spant671@gmail.com>
2026-07-16 01:37:03 +0530

    comparison

--- Learning Javascript/01_basics/03_conversionOperation.js
+++ Learning Javascript/01_basics/03_conversionOperation.js
@@ -1,20 +1,20 @@
 let score = "33abc"
 
-console.log(typeof score) //string
+// console.log(typeof score) //string
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber) //number
-console.log(valueInNumber) //NaN
+// console.log(typeof valueInNumber) //number
+// console.log(valueInNumber) //NaN
 
-let value = null
-console.log(value)
-let noValue = Number(value)
-console.log(noValue) //0
+let input = null
+// console.log(input) //null
+let noInput = Number(input)
+// console.log(noInput) //0
 
 let def = undefined
-console.log(def) //undefined
+// console.log(def) //undefined
 let nodef = Number(def)
-console.log(nodef) //NaN
+// console.log(nodef) //NaN
 
 /*
 "33" => 33
@@ -25,7 +25,7 @@ true => 1; false => 0
 let isLoggedIn= 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn) //true
+// console.log(booleanIsLoggedIn) //true
 
 /*
 1 => true; 0 => false
@@ -35,6 +35,42 @@ console.log(booleanIsLoggedIn) //true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
 
+// ********************************* Operations ******************************************//
 
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+*/
+
+let str1 = "hello"
+let str2 = " bro"
+let str3 = str1 + str2
+console.log(str3)
+
+/*
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+*/
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
