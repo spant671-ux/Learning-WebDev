b94026e667ed52cc299f529bb0605179a55f77a9
Santosh <spant671@gmail.com>
2026-07-15 19:11:53 +0530

    started operators and conditionals

---
+++ Learning Javascript/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553;
+let accountEmail = "santoshpant@gmail.com";
+var accountPassword = "12345";
+accountCity = "Pithoragarh";
+
+//accountId = 2;
+console.log(accountId);
+accountEmail = "sp@sp.com";
+accountPassword= "21212121";
+accountCity= "Bengaluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
