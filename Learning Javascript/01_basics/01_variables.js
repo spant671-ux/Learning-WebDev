acbffecd68e7e111d26e9c91fa62d210b122db6d
Santosh <spant671@gmail.com>
2026-07-16 00:28:56 +0530

    started datatypes

--- Learning Javascript/01_basics/01_variables.js
+++ Learning Javascript/01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553;
 let accountEmail = "santoshpant@gmail.com";
 var accountPassword = "12345";
 accountCity = "Pithoragarh";
+let accountState;
 
 //accountId = 2;
 console.log(accountId);
@@ -9,4 +10,7 @@ accountEmail = "sp@sp.com";
 accountPassword= "21212121";
 accountCity= "Bengaluru";
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+/*
+Prefer not to use var because of issues in block and functional scopes
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
