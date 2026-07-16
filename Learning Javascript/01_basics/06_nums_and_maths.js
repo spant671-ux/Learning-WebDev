c71646505f3658d929ce48b1becd133f5b3eca40
Santosh <spant671@gmail.com>
2026-07-17 02:12:04 +0530

    nums and maths

---
+++ Learning Javascript/01_basics/06_nums_and_maths.js
@@ -0,0 +1,8 @@
+const score = 400
+console.log(score) //400
+
+const balance = new Number(100)
+console.log(balance) //[Number: 100]
+
+console.log(balance.toString().length) //3
+console.log(balance.toFixed(2)); //100.00
