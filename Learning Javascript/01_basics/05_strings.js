920e8e888a56de633d31adb9605bc76a08840891
Santosh <spant671@gmail.com>
2026-07-17 01:51:15 +0530

    strings

--- Learning Javascript/01_basics/05_strings.js
+++ Learning Javascript/01_basics/05_strings.js
@@ -5,12 +5,26 @@ const repoCount= 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 
-const gameName = new String('mikey')    
+const gameName = new String('mikey-sp')    
 // console.log(gameName)
-console.log(gameName[0]) //m
-console.log(gameName.__proto__) //{}
-console.log(gameName.length) //5
+// console.log(gameName[0]) //m
+// console.log(gameName.__proto__) //{}
+// console.log(gameName.length) //8
 
-console.log(gameName.toUpperCase()) //MIKEY
+// console.log(gameName.toUpperCase()) //MIKEY
 console.log(gameName.charAt(2)) //k
+console.log(gameName.indexOf('e')) //3
+
+const newString = gameName.substring(0, 4)
+console.log(newString) //mike
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString) //ike
+
+const newStringOne = "    sans    "
+console.log(newStringOne) //    sans
+console.log(newStringOne.trim()); //sans
+
+
+
 
