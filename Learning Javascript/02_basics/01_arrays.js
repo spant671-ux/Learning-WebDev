3361130236f910a87931e9860eb60a2d143ada6d
Santosh <spant671@gmail.com>
2026-07-18 01:48:09 +0530

    arrays

--- Learning Javascript/02_basics/01_arrays.js
+++ Learning Javascript/02_basics/01_arrays.js
@@ -1,1 +1,20 @@
 //arrays
+
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["spiderman", "batman", "superman"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1])
+
+// array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()   
+
+console.log(myArr.includes(9));
+
+console.log(myArr)
+
