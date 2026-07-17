fefe8ed3ea2005707cacbe430ad10be74c70f818
Santosh <spant671@gmail.com>
2026-07-18 02:14:42 +0530

    arrays

--- Learning Javascript/02_basics/01_arrays.js
+++ Learning Javascript/02_basics/01_arrays.js
@@ -29,3 +29,8 @@ console.log("A ", myArr)
 const myn1 = myArr.slice(1, 3)
 console.log(myn1)
 console.log("B ", myArr)
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr)
+
+console.log(myn2)
