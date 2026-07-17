573a30bce9a8059c3ae4ecf65b77f3f3f2c4e94e
Santosh <spant671@gmail.com>
2026-07-18 02:09:54 +0530

    arrays

--- Learning Javascript/02_basics/01_arrays.js
+++ Learning Javascript/02_basics/01_arrays.js
@@ -3,7 +3,7 @@
 const myArr = [0,1,2,3,4,5]
 const myHeroes = ["spiderman", "batman", "superman"]
 const myArr2 = new Array(1,2,3,4)
-console.log(myArr[1])
+// console.log(myArr[1])
 
 // array methods
 
@@ -14,7 +14,18 @@ console.log(myArr[1])
 // myArr.unshift(9)
 // myArr.shift()   
 
-console.log(myArr.includes(9));
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
 
-console.log(myArr)
+// const newArr = myArr.join()
 
+// console.log(myArr)
+// console.log(newArr)
+
+// slice, splice
+
+console.log("A ", myArr)
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1)
+console.log("B ", myArr)
