f94089b6d5b0fe733a2c2cd4a1c7b1f8d0d02308
Santosh <spant671@gmail.com>
2026-07-18 18:49:51 +0530

    arrays

--- Learning Javascript/02_basics/02_arrays.js
+++ Learning Javascript/02_basics/02_arrays.js
@@ -5,5 +5,26 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros)
 // console.log(marvel_heros[3][1])
 
-const allHeros = marvel_heros.concat(dc_heros)
-console.log(allHeros)
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+const allNewHeros = [...marvel_heros, ...dc_heros]
+// console.log(allNewHeros)
+
+const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]] 
+
+const realAnotherArray = anotherArray.flat(Infinity)
+console.log(realAnotherArray)
+
+console.log(Array.isArray("Sans"))
+console.log(Array.from("Sans")) 
+console.log(Array.from({name: "Sans"})) //interesting case for interviews
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
+
+
+
