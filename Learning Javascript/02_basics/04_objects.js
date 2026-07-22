0d3dac57614cc7ff2d0748af2b10503dc59e4f80
Santosh <spant671@gmail.com>
2026-07-22 17:34:39 +0530

    objects

--- Learning Javascript/02_basics/04_objects.js
+++ Learning Javascript/02_basics/04_objects.js
@@ -1,11 +1,18 @@
-//const tinderUser = new Object()
-const tinderUser = {}
+// ==================== Objects (Singleton, Nesting, Merging & Methods) ====================
+
+// 1. Singleton vs Non-Singleton Declaration
+// const tinderUser = new Object() // Singleton object (created via Constructor)
+const tinderUser = {} // Non-singleton object (created via Object Literal)
+
+// Dynamically adding properties to an object:
 tinderUser.id = "123abc"
 tinderUser.name = "Sammie"
 tinderUser.isLoggedIn = false
- 
-//console.log(tinderUser)
 
+// console.log(tinderUser)
+
+
+// 2. Nested Objects
 const regularUser = {
     email: "some@gmail.com",
     fullName: {
@@ -16,18 +23,30 @@ const regularUser = {
     }
 }
 
-//console.log(regularUser.fullName.userfullname.firstname)
+// Accessing deeply nested properties using dot notation:
+// console.log(regularUser.fullName.userfullname.firstname)
+
+
+// ==================== Merging Objects ====================
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
+// Note: {obj1, obj2} nests objects inside a new object rather than merging their properties.
+// const obj3 = {obj1, obj2}
 
-//const obj3 = {obj1, obj2}
-//const obj3 = Object.assign({}, obj1, obj2, obj4)
+// Object.assign(target, ...sources): Merges source objects into target.
+// Using empty object {} as target prevents mutating obj1.
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
 
+// Spread Operator (...): Modern & preferred syntax for combining object properties.
 const obj3 = {...obj1, ...obj2, ...obj4}
 // console.log(obj3)
+
+
+// ==================== Array of Objects ====================
+// Commonly received as response data from APIs and databases
 
 const users = [
     {
@@ -44,13 +63,21 @@ const users = [
     }
 ]
 
+// Accessing an object inside an array by index and then targeting its property:
 users[1].email
 console.log(tinderUser)
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
+
+// ==================== Object Class Utility Methods ====================
+
+// Object.keys(obj): Returns an array containing all property key names of the object
+console.log(Object.keys(tinderUser)) // Output: ['id', 'name', 'isLoggedIn']
+
+// Object.values(obj): Returns an array containing all property values of the object
+console.log(Object.values(tinderUser)) // Output: ['123abc', 'Sammie', false]
+
+// Object.entries(obj): Returns an array of key-value pairs formatted as [key, value] arrays
 console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
-
-
+// hasOwnProperty(propName): Returns boolean indicating whether object has specified property directly
+console.log(tinderUser.hasOwnProperty('isLoggedIn')) // Output: true
