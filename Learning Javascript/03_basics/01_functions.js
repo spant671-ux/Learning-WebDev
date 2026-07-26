dd1f54c25dfce9ca4b834e1abbedcb01abb64b4f
Santosh <spant671@gmail.com>
2026-07-27 00:45:12 +0530

    functions

---
+++ Learning Javascript/03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("s")
+console.log("a")
+console.log("n")
+console.log("s")
+}
+
+sayMyName() //sayMyName reference, () execution
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2)
+}
+
+addTwoNumbers(3, 5) //8
