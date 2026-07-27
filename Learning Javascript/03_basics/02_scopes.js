1e5eb2f793b3bbe188376ae794e2346075b38db4
Santosh <spant671@gmail.com>
2026-07-27 18:32:00 +0530

    scopes

---
+++ Learning Javascript/03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// var c= 300
+let a = 300
+if(true) {
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Inner:", a)
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
