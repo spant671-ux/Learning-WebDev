8708959cb03289df235b93f269cd51679911b014
Santosh <spant671@gmail.com>
2026-07-31 22:22:50 +0530

    if else if

--- Learning Javascript/04_control_flow/one.js
+++ Learning Javascript/04_control_flow/one.js
@@ -16,19 +16,26 @@
 //     console.log(`User power: ${power}`)
 // }
 
-const balance = 1000
+// const balance = 1000
 // if(balance > 500) console.log("test"); //implicit scope
 
-if (balance < 500){
-    console.log("less than 500")
-}
-else if (balance < 750){
-    console.log("less than 750")
-}
-else if (balance < 950){
-    console.log("less than 950");
-}
-else{
-    console.log("less than 1200");
-}
+// if (balance < 500){
+//     console.log("less than 500")
+// }
+// else if (balance < 750){
+//     console.log("less than 750")
+// }
+// else if (balance < 950){
+//     console.log("less than 950");
+// }
+// else{
+//     console.log("less than 1200");
+// }
 
+const userLoggedIn = true
+const debitCard = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy courses");
+    
+}
