d117a610c1f6e3ee849a93982e85ca1f85602885
Santosh <spant671@gmail.com>
2026-07-31 22:20:17 +0530

    if else if

--- Learning Javascript/04_control_flow/one.js
+++ Learning Javascript/04_control_flow/one.js
@@ -17,5 +17,12 @@
 // }
 
 const balance = 1000
-if(balance > 500) console.log("test"); //implicit scope
+// if(balance > 500) console.log("test"); //implicit scope
+
+if (balance < 500){
+    console.log("less than 500")
+}
+else if (balance < 750){
+    console.log("less than 750")
+}
 
