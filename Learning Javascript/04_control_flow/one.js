e78fa0adfbc979dcd092c3f3f2ea643bd74afe53
Santosh <spant671@gmail.com>
2026-07-31 22:25:11 +0530

    if else if

--- Learning Javascript/04_control_flow/one.js
+++ Learning Javascript/04_control_flow/one.js
@@ -34,8 +34,14 @@
 
 const userLoggedIn = true
 const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
 
 if(userLoggedIn && debitCard){
-    console.log("Allow to buy courses");
+    console.log("Allow to buy courses"); 
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User Logged In");
     
 }
