e31911d4f22170a1a43ac07385e47b9e7eaca490
Santosh <spant671@gmail.com>
2026-08-01 20:39:32 +0530

    for loops

--- Learning Javascript/05_iterations/one.js
+++ Learning Javascript/05_iterations/one.js
@@ -1,6 +1,17 @@
 //for
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element==5){
+        // console.log("5 is best number");
+    }
+    console.log(element);
+}
+
+// console.log(element);
+
+for (let i = 0; i <= 10; i++) {
+    for(let j = 0; j <= 10; j++){
+        
+    }
 }
