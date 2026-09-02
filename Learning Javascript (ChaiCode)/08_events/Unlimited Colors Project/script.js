8eeb4ca6b2dc0366812d8c0db9bbd6d406a6c54b
Santosh <spant671@gmail.com>
2026-09-03 04:07:08 +0530

    colors

--- Learning Javascript (ChaiCode)/08_events/Unlimited Colors Project/script.js
+++ Learning Javascript (ChaiCode)/08_events/Unlimited Colors Project/script.js
@@ -10,13 +10,16 @@ const randomColor = function(){
 
 let intervalId;
 const startChangingColor = function(){
-    intervalId = setInterval(changeBgColor, 1000); 
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor, 1000);
+    } 
     function changeBgColor(){
         document.body.style.backgroundColor = randomColor()
     }
 }
 const stopChangingColor = function(){
-    clearInterval(intervalId)
+    clearInterval(intervalId);
+    intervalId = null
 }
 document.querySelector('.start').addEventListener('click', startChangingColor)
 document.querySelector('.stop').addEventListener('click', stopChangingColor)
